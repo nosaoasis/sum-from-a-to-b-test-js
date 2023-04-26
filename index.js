8857503525dca85c6464a27b14527aa9c4cf7574
Nosa Agho <nosaoasis@gmail.com>
2023-04-26 19:35:17 +0100

    Sum Using Recursion
    Implemented the function 'sum' which takes two integers as parameters and returns the sum of the numbers between the both integers. The function is implemented using recursion.

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 
 function sum(fromN, toN) {
   // Sum all the values from fromN up to toN
+  let x = fromN
+  if (x === toN) return toN
+  return fromN + sum((fromN + 1), toN)
 }
+
+console.log(sum(2, 5))
 
 module.exports = sum;
